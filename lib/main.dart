d07753fdc361e8fbb4832e66c3fb846dd7afe59c
Arijit-Biswas-Anirban <arijit.cse.pust27@gmail.com>
2024-05-22 23:59:28 +0800

    adding firebase and basic file

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: unnecessary_null_comparison, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:untitled/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(ProviderScope(child: MyApp()));
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
